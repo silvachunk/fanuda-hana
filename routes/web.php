d59eb2368878a9963cfefbe2264154937eeb484a
Silva <silva@mirainu.com>
2025-06-10 13:42:53 -0700

    Fix: Added POST route for Hana AI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HanaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/hana/respond', [HanaController::class, 'respond']);

d25660712acead492b91e64b9c3627c86a857a7d
Silva <silva@mirainu.com>
2025-06-12 14:19:01 -0700

    Added Hana /respond route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HanaController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API file is working!']);
 });
+
+Route::post('/hana/respond', [HanaController::class, 'respond']);
